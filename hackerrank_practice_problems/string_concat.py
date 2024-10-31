ac416e40181fe586178d9a1d8a4b3b309c898312
misael-b <142462435+misael-b@users.noreply.github.com>
2024-10-31 23:25:28 +0000

    practice problems

---
+++ hackerrank_practice_problems/string_concat.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+  n = int(input())
+  ans = ""
+  for i in range(1, n+1):
+      ans += str(i)
+  print(ans)
+
+
+# for i in range(n):
+    # i=i+1
+    # print(i,end="")
+ # end = "" prevents new line
